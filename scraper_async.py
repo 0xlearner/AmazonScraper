29ec76bbeb815b1e8bf1e9ce883d44b995bc0918
zeroxlearner <zeroxlearner@gmail.com>
2022-02-03 01:14:09 +0500

    Async - Amazon Scraper API

--- scraper_async.py
+++ scraper_async.py
@@ -1,7 +1,8 @@
 import time
 import json
 from datetime import datetime
-from requests_html import HTMLSession
+from requests_html import AsyncHTMLSession
+import asyncio
 from amazon_config import (
     DIRECTORY,
     NAME,
@@ -9,6 +10,9 @@ from amazon_config import (
     FILTERS,
     BASE_URL,
 )
+
+TIMEOUT = 240
+RETRIES = 4
 
 
 class GenerateReport:
@@ -23,6 +27,7 @@ class GenerateReport:
         report = {
             "title": self.file_name,
             "date": self.get_now(),
+            "product_count": len(self.data),
             "best_item": self.get_best_item(),
             "currency": self.currency,
             "filters": self.filters,
@@ -41,9 +46,7 @@ class GenerateReport:
 
     def get_best_item(self):
         try:
-            return sorted(
-                self.data["products"], key=lambda k: k["rating"], reverse=True
-            )[0]
+            return sorted(self.data, key=lambda x: x["rating"], reverse=True)[0]
         except Exception as e:
             print(e)
             print("Problem with sorting items")
@@ -52,35 +55,43 @@ class GenerateReport:
 
 class AmazonAPI:
     def __init__(self, search_term, filters, base_url, currency):
-        self.session = HTMLSession()
+        self.asession = AsyncHTMLSession()
         self.headers = {
-            "USer-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0",
+            "Accept": "text/html,*/*",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br",
+            "X-Requested-With": "XMLHttpRequest",
+            "Connection": "keep-alive",
+            "Referer": f"https://www.amazon.com/s?k={search_term}&ref=nb_sb_noss",
         }
         self.base_url = base_url
         self.search_term = search_term
         self.currency = currency
         self.price_filter = f"&rh=p_36%3A{filters['min']}00-{filters['max']}00"
 
-    def run(self):
+    async def run(self):
         print("Strating script...")
         print(f"Looking for {self.search_term} products...")
-        urls = self.get_product_links()
+        urls = await self.get_product_links()
         if not urls:
             print("Stopped script.")
             return
         print(f"Got {len(urls)} products.")
         print("Parsing the product links...")
-        products = [self.parse_urls(url) for url in urls]
-        print(f"Got information about {len(products)} products.")
-        return products
+        tasks = [asyncio.create_task(self.parse_urls(url)) for url in urls]
+        # print(f"Got information about {len(products)} products.")
+        result = await asyncio.gather(*tasks)
+        return result
 
-    def get_product_links(self):  #
+    async def get_product_links(self):  #
         # https://www.amazon.com/s?k=ps5&rh=p_36%3A27500-65000
-        url = self.session.get(
+        url = await self.asession.get(
             self.base_url + self.search_term + self.price_filter,
             headers=self.headers,
         )
-        url.html.render(timeout=60)
+        # await url.html.arender(retries=RETRIES, timeout=TIMEOUT)
+        print(url.status_code)
         product_asins = [
             asin.attrs["data-asin"]
             for asin in url.html.find("div[data-asin]")
@@ -91,25 +102,25 @@ class AmazonAPI:
         product_link = ["https://www.amazon.com/dp/" + link for link in product_asins]
         return product_link
 
-    def parse_urls(self, url):
-        _session = self.session.get(url, headers=self.headers)
-        _session.html.render(timeout=60)
+    async def parse_urls(self, url):
+        _asession = await self.asession.get(url, headers=self.headers)
+        # await _asession.html.arender(retries=RETRIES, timeout=TIMEOUT)
         print(f"Getting Product:{url} --- Data")
 
         product_asin = url.split("/")[4]
         print("ASIN:", product_asin)
-        product_title = self.get_title(url)
-        print("TITLE:", product_title)
-        product_price = self.get_price(url)
-        print("PRICE:", product_price)
-        product_seller = self.get_seller(url)
-        print("SELLER:", product_seller)
-        product_review_count = self.get_review_count(url)
-        print("REVIEW_COUNT:", product_review_count)
-        product_rating = self.get_rating(url)
-        print("RATING:", product_rating)
-        product_photo_url = self.get_photo_url(url)
-        print("PHOTO_URL:", product_photo_url)
+        product_title = await self.get_title(url)
+        print(f"TITLE: {product_title} --- {url}")
+        product_price = await self.get_price(url)
+        print(f"PRICE: {product_price} --- {url}")
+        product_seller = await self.get_seller(url)
+        print(f"SELLER: {product_seller} --- {url}")
+        product_review_count = await self.get_review_count(url)
+        print(f"REVIEW_COUNT: {product_review_count} --- {url}")
+        product_rating = await self.get_rating(url)
+        print(f"RATING: {product_rating} --- {url}")
+        product_photo_url = await self.get_photo_url(url)
+        print(f"PHOTO: {product_photo_url} --- {url}")
         if product_title and product_price and product_rating:
             product_info = {
                 "asin": product_asin,
@@ -125,127 +136,153 @@ class AmazonAPI:
             return product_info
         return None
 
-    def get_title(self, url):
-        _session = self.session.get(url, headers=self.headers)
-        _session.html.render(timeout=60)
+    async def get_title(self, url):
+        _asession = await self.asession.get(url, headers=self.headers)
+        # await _asession.html.arender(retries=RETRIES, timeout=TIMEOUT)
+        title = None
 
         try:
-            return _session.html.find("span#productTitle", first=True).text
+            if _asession.html.find("span#productTitle", first=True):
+                title = _asession.html.find("span#productTitle", first=True).text
+                return title
+            elif _asession.html.find("h1.a-size-large", first=True):
+                title = _asession.html.find("h1.a-size-", first=True).text
+                return title
+            else:
+                title = ""
+
         except Exception as e:
             print(e)
             print(f"Can't get a title of a product - {url}")
             return None
 
-    def get_seller(self, url):
-        _session = self.session.get(url, headers=self.headers)
-        _session.html.render(timeout=60)
+    async def get_seller(self, url):
+        _asession = await self.asession.get(url, headers=self.headers)
+        # await _asession.html.arender(retries=RETRIES, timeout=TIMEOUT)
         try:
-            return _session.html.find("a#bylineInfo", first=True).text
-        except Exception as e:
-            print(e)
-            print(f"Can't get a seller of a product - {url}")
-            return None
+            seller = _asession.html.find("a#bylineInfo", first=True).text
+            if "Visit the" in seller:
+                return " ".join(seller.split(" ")[2:])
+            elif "Brand" or "Brand: " in seller:
+                return "".join(seller.split(" ")[0:])
 
-    def get_price(self, url):
-        price = None
-        _session = self.session.get(url, headers=self.headers)
-        _session.html.render(timeout=60)
-        try:
-            price_1 = float(_session.html.find("span.apexPriceToPay", first=True))
-            if price_1 is not None:
-                price = price_1.text.split("$")[1]
-            else:
-                price = float(
-                    _session.html.find(
-                        "span#priceblock_ourprice", first=True
-                    ).text.split("$")[1]
-                )
         except Exception as e:
             print(e)
             try:
-                availability = _session.html.find("span.a-color-price", first=True).text
-                out_of_stock = _session.html.find("span.a-color-price", first=True).text
-                availability_message = _session.html.find(
+                return _asession.html.find("a.qa-byline-url", first=True).text
+            except Exception as e:
+                print(f"Can't get a seller of a product - {url}")
+                return None
+
+    async def get_price(self, url):
+        price = None
+        _asession = await self.asession.get(url, headers=self.headers)
+        # await _asession.html.arender(retries=RETRIES, timeout=TIMEOUT)
+        try:
+            price_1 = _asession.html.find("span.apexPriceToPay", first=True)
+            if price_1 is not None:
+                price = float(price_1.text.split("$")[1])
+            else:
+                price = float(
+                    _asession.html.find(
+                        "span#priceblock_ourprice", first=True
+                    ).text.split("$")[1]
+                )
+                print(price)
+        except Exception as e:
+            print(e)
+            try:
+                availability = _asession.html.find(
+                    "span.a-color-price", first=True
+                ).text
+                out_of_stock = _asession.html.find(
+                    "span.a-color-price", first=True
+                ).text
+                availability_message = _asession.html.find(
                     "span.qa-availability-message", first=True
                 ).text
                 if "In Stock." or None in availability:
                     price = float(
-                        _session.html.find(
+                        _asession.html.find(
                             "span.apexPriceToPay", first=True
                         ).text.split("$")[1]
                     )
                 elif "Temporarily out of stock." in out_of_stock:
-                    price = out_of_stock
+                    price = 0
                 elif "Currently unavailable." in availability:
-                    price = availability.split("\n")[0]
+                    price = 0
                 elif "Currently unavailable." in availability_message:
-                    price = availability_message.split("\n")[0]
+                    price = 0
+                    print(price)
                 elif "Currently unavailable." in out_of_stock:
-                    price = out_of_stock
+                    price = 0
                 else:
                     price = None
             except Exception as e:
                 print(price)
                 if not price:
                     try:
-                        price = _session.html.find(
-                            "span.a-color-price:nth-child(2)", first=True
-                        ).text.split("$")[1]
+                        price = float(
+                            _asession.html.find(
+                                "span.a-color-price:nth-child(2)", first=True
+                            ).text.split("$")[1]
+                        )
                     except Exception as e:
                         availability = None
-                        if _session.html.find(
+                        if _asession.html.find(
                             "div#availability", first=True
                         ).text.startswith("P"):
                             availability = "N/A"
                             print(f"Product {availability}")
                         elif (
                             "In Stock."
-                            in _session.html.find("div#availability", first=True).text
+                            in _asession.html.find("div#availability", first=True).text
                         ):
-                            price = _session.html.find(
-                                "div#availability", first=True
-                            ).text
+                            price = 0
                         else:
-                            availability = _session.html.find(
+                            availability = _asession.html.find(
                                 "div#availability", first=True
                             ).text.split("\n")[0]
                             print(f"Product {availability}")
 
                         return None
-        return price
+        return float(price)
 
-    def get_review_count(self, url):
-        _session = self.session.get(url, headers=self.headers)
-        _session.html.render(timeout=60)
+    async def get_review_count(self, url):
+        _asession = await self.asession.get(url, headers=self.headers)
+        # await _asession.html.arender(retries=RETRIES, timeout=TIMEOUT)
 
         try:
-            return _session.html.find(
-                "span#acrCustomerReviewText", first=True
-            ).text.split(" ")[0]
+            return int(
+                _asession.html.find("span#acrCustomerReviewText", first=True)
+                .text.split(" ")[0]
+                .replace(",", "")
+                .strip()
+            )
         except Exception as e:
             print(e)
             print(f"Can't get a review count of a product - {url}")
             return None
 
-    def get_rating(self, url):
-        _session = self.session.get(url, headers=self.headers)
-        _session.html.render(timeout=60)
+    async def get_rating(self, url):
+        _asession = await self.asession.get(url, headers=self.headers)
+        # await _asession.html.arender(retries=RETRIES, timeout=TIMEOUT)
 
         try:
             return float(
-                _session.html.find("span.a-icon-alt", first=True).text.split(" ")[0]
+                _asession.html.find("span.a-icon-alt", first=True).text.split(" ")[0]
             )
         except Exception as e:
             print(e)
             print(f"Can't get a review of a product - {url}")
             return None
 
-    def get_photo_url(self, url):
-        _session = self.session.get(url, headers=self.headers)
-        _session.html.render(timeout=60)
+    async def get_photo_url(self, url):
+        _asession = await self.asession.get(url, headers=self.headers)
+        # await _asession.html.arender(retries=RETRIES, timeout=TIMEOUT)
 
         try:
-            return _session.html.find("img#landingImage", first=True).attrs["src"]
+            return _asession.html.find("img#landingImage", first=True).attrs["src"]
         except Exception as e:
             print(e)
             print(f"Can't get a photo url of a product - {url}")
@@ -271,8 +308,9 @@ def remove_empty_elements(d):
 
 
 if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
     am = AmazonAPI(NAME, FILTERS, BASE_URL, CURRENCY)
-    scraped_data = am.run()
+    scraped_data = loop.run_until_complete(am.run())
     data = remove_empty_elements(scraped_data)
     print(data)
     GenerateReport(NAME, FILTERS, BASE_URL, CURRENCY, data)
